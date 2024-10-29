3f669a2190216060350aa51e1c169c836e7113e7
kozerd <kozerdominik@gmail.com>
2024-10-29 11:06:05 +0100

    NgFor zmiany obiektowe

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {ToDo} from '../shered/interfaces/todo.interface';
+
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-  todos : string[] = [];
+  todos : ToDo[] = [];
 
   addTodo(todo:string) : void {
-    this.todos.push(todo);
+    this.todos.push({name : todo,isComplete: true});
     console.log('Akt list todo: ',this.todos);
   }
   // defaultValue = "Test"; tu mozna dac jakis tekst
